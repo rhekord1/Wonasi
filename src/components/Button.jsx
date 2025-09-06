e400327998ba72a4b5153096b4ba5ac7424a469d
rhekord <rhekord1@gmail.com>
2025-09-06 04:04:00 +0000

    Modified Button.jsx -added url condition statement to the component props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,9 @@ const Button = ( {label, iconUrl} ) => {
      border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white 
      border-coral-red">
         {label}
+        { iconUrl && 
         <img src={iconUrl} alt="icon" className="ml-2 rounded-full w-5 h-5 " />
+        }
     </button>
   )
 }
