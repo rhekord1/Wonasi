450bcde10b8d32c8f5a5a210a38fba0df3d0a6c2
rhekord <rhekord1@gmail.com>
2025-09-09 16:22:17 +0000

    Modified SpecialOffer.jsx -added buttons and paragraphs

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,8 +1,33 @@
-
+import Button from "../components/Button";
+import {offer} from "../assets/images";
+import { arrowRight } from "../assets/icons";
 
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+    <section className = "flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <img src={offer} width={773} height={687}  className="object-contain w-full"/>
+      </div>
+      <div className = "flex flex-1 flex-col">
+     
+              <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg" >  
+                 <span className="text-coral-red ">Special </span> Offer
+              </h2>
+              <p className="mt-4 lg:max-w-lg info-text"> Ensuring premium comfort and style, our meticulous craftsmanship guarantees a perfect fit and provides long-lasting durability.
+                Ensuring premium comfort and style, our meticulous craftsmanship guarantees a perfect fit and provides long-lasting durability.
+              </p>
+              <p className = "mt-6 lg:max-w-lg info-text">
+                Our dedication to quality means every shoe is designed to meet the highest standards, offering you a blend of elegance and performance.
+              </p>
+              <div className="mt-11 flex flex-wrap gap-4" >
+              <Button label="Shop now " iconUrl={arrowRight}/>
+              <Button label="Learn more" 
+              backgroundColor="bg-white"  
+              borderColor="border-slate-gray"  
+              textColor="text-slate-gray"/>
+              </div>
+      </div>
+    </section>
   )
 }
 
