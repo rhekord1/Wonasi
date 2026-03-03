8f45504c85831d24890492ce4f17698f66a1357e
rhekord <rhekord1@gmail.com>
2026-03-03 17:05:34 +0000

    logo changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,43 +1,31 @@
-import {headerLogo} from '../assets/images';
-import {hamburger} from '../assets/icons';
+import { hamburger } from '../assets/icons';
 import { navLinks } from '../constants';
-
 
 const Nav = () => {
   return (
     <header className='padding-x py-8 absolute z-10 w-full'>
-        <nav className='flex justify-between items-center max-container'>
-            <a href="/" >
-            <img src={headerLogo} 
-            alt='Logo'
-            width={130}
-            height={29}  
-            className='m-0 w-[129px] h-[29px]'
-            />
-            </a>
-            <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
-                {navLinks.map((item) => (
-
-                    <li key={item.label}>
-                       <a href={item.href}  className='font-monserrat loading-normal text-lg text-slate-gray'>
-                        {item.label}
-                        </a>
-                    </li>
-
-                ))}
-
-            </ul>
-            <div className='hidden max-lg:block '>
-                <img src={hamburger} 
-                alt="hamburger" 
-                width={25}
-                height={25}
-                />
-            </div>
-
-        </nav>
+      <nav className='flex justify-between items-center max-container'>
+        <a href="/" className="flex items-center gap-2">
+          {/* Wonasi wordmark replacing logo image */}
+          <span className="text-2xl font-bold font-palanquin" style={{ color: "#FF6B6B" }}>
+            Wonasi
+          </span>
+        </a>
+        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a href={item.href} className='font-montserrat leading-normal text-lg text-slate-gray'>
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className='hidden max-lg:block'>
+          <img src={hamburger} alt="hamburger" width={25} height={25} />
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
 export default Nav;
